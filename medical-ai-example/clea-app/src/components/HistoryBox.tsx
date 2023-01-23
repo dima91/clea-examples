4cfaa02133e3d04b6e189b32747e4288c6ed49fb
Luca Di Mauro <dima.91@live.it>
2023-01-23 12:08:28 +0100

    [MED_AI] Fixed HistoryBox pages count

--- medical-ai-example/clea-app/src/components/HistoryBox.tsx
+++ medical-ai-example/clea-app/src/components/HistoryBox.tsx
@@ -12,12 +12,38 @@ type HistoryBoxProps = {
     roomsDescriptors    : Array<RoomDescriptor>
 };
 
+type ButtonDescriptor = {
+    char    : String
+    pageIdx : Number | undefined
+    onClick : () => void
+}
+
+
+const computeShownPageRange = (minPage:number, maxPage:number, currPage:number, pageDelta:number) : number[] => {
+    let lPage   = minPage
+    let rPage   = lPage+pageDelta*2;
+    rPage       = rPage<=maxPage ? rPage : maxPage
+        while (lPage+pageDelta<currPage){
+            lPage++;
+            rPage++;
+        }
+        if (maxPage-currPage<=pageDelta) {
+            rPage   = maxPage
+            lPage   = (rPage-pageDelta*2)-1
+            lPage   = rPage<=maxPage ? lPage : minPage
+        }
+
+
+    return [lPage, rPage]
+}
+
 
 const HistoryBox : React.FC<HistoryBoxProps> = ({events, selectedRoomIdx, focusDescriptorIdx, roomsDescriptors}) => {
     
-    const ITEMS_PER_PAGE    = 8
-    const START_PAGE        = 0
-
+    const ITEMS_PER_PAGE        = 8
+    const START_PAGE            = 0
+    const BUTTONS_PAGES_DELTA   = 2
+    
     let [focusedPageIndex, setFocusedPageIndex] = React.useState (START_PAGE)
     let revEvents : Event[]                     = []
     let patientMap : Map<number, number>        = new Map()
@@ -40,7 +66,7 @@ const HistoryBox : React.FC<HistoryBoxProps> = ({events, selectedRoomIdx, focusD
         let status  = stringToPatientStatus (item.eventType)
         let evts    = roomEvents.get(item.roomId)
         if (evts==undefined)
-            throw (`Events array for room #${item.roomId} is undefined`)
+        throw (`Events array for room #${item.roomId} is undefined`)
         evts.push (item)
 
         if (selectedRoomIdx == -1) {
@@ -78,36 +104,41 @@ const HistoryBox : React.FC<HistoryBoxProps> = ({events, selectedRoomIdx, focusD
         }
     }
     let integerDivision                         = (a:number, b:number) => Math.floor (a/b) + (a%b==0?0:1)
-    let buttonsDescriptors                      = [
+    let buttonsDescriptors : ButtonDescriptor[] = [
         {
             char    : '<',
+            pageIdx : undefined,
             onClick : () => {setFocusedPageIndex((oldV) => oldV>0 ? oldV-1 : oldV)}
         },
         {
             char    : '>',
+            pageIdx : undefined,
             onClick : () => {setFocusedPageIndex((oldV) => oldV<pagesCount-1 ? oldV+1 : oldV)}
         }
     ]
-
+    
     events?.map( (item, idx, array) => {
         if (selectedRoomIdx == -1 || selectedRoomIdx == item.roomId)
-            revEvents.unshift(item)
+        revEvents.unshift(item)
     })
-
+    
     let pagesCount  = integerDivision (revEvents.length, ITEMS_PER_PAGE)
     for (let i=0; i<pagesCount; ++i) {
         buttonsDescriptors.splice (i+1, 0, {
             char    : (i+1).toString(),
+            pageIdx : i,
             onClick : () => {setFocusedPageIndex(i)}
         })
     }
+    
+    let [MIN_PAGE, MAX_PAGE]  = computeShownPageRange (0, buttonsDescriptors.length-2, focusedPageIndex, BUTTONS_PAGES_DELTA)
     
     
     roomsDescriptors.map ((v, i, a) => {
         patientMap.set (v.roomId, v.patientId)
         roomEvents.set (v.roomId, [])
     })
-
+    
     React.useEffect (() => {
         setFocusedPageIndex (START_PAGE)
     }, [selectedRoomIdx])
@@ -185,13 +216,18 @@ const HistoryBox : React.FC<HistoryBoxProps> = ({events, selectedRoomIdx, focusD
 
                                 <div className="d-flex justify-content-end">
                                     {buttonsDescriptors.map ((item, idx, arr) => {
-                                        return (
+                                        let nIdx    = idx-1;                                        
+
+                                        return item.pageIdx == undefined || (MIN_PAGE <= nIdx && nIdx <= MAX_PAGE) ?
+                                        (
                                             <Button className="btn-circle shadow ml-4 mr-2"
-                                                            variant={idx-1 == focusedPageIndex ? 'primary' : 'light'}
+                                                            variant={nIdx == focusedPageIndex ? 'primary' : 'light'}
                                                             onClick={item.onClick}>
                                                 {item.char}
                                             </Button>
                                         )
+                                        :
+                                        <></>
                                     })}
                                 </div>
                             </Card.Body>
