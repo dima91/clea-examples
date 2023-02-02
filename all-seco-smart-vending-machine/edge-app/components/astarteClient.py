9dcaa6e6e0ab7ff7dfb09054a4c5017375b53f03
Luca Di Mauro <dima.91@live.it>
2023-02-02 17:55:47 +0100

    [ALL_SVM] Implemented connection and disconnection of AstarteClient

--- all-seco-smart-vending-machine/edge-app/components/astarteClient.py
+++ all-seco-smart-vending-machine/edge-app/components/astarteClient.py
@@ -1,16 +1,73 @@
 
+import os
+from astarte.device import Device
+from PySide6.QtCore import QObject, Signal
 
-from astarteClient import AstarteClient
-from PySide6.QtCore import Signal
+class AstarteClient(QObject) :
 
-class AstarteClient() :
-
-    # Defining emitted signals
+    ## Public members
     NewConnectionStatus = Signal (bool)
-    
+    IncomingData        = Signal (any)
+
+
+    ## Private members
+    __device    = None
+    __loop      = None
+
 
     def __init__(self, config, loop) -> None:
-        # TODO Initializing Astarte object
+        astarte_config  = config['astarte']
+
+        super().__init__()
+
+        self.__loop     = loop
+
+        # Checking direcotry existence
+        persistency_path    = astarte_config['persistency_dir']
+        if not os.path.exists(persistency_path) :
+            print ("Directory at path "+persistency_path+" does not exists.\nCreating it...")
+            os.mkdir (persistency_path)
+        elif not os.path.isdir (persistency_path) :
+            error_message   = f"File at path {persistency_path} is not a directory"
+            print (error_message)
+            raise Exception (error_message)
+
+        # Initializing Astarte object
+        #   Assigning   loop=None -> callbacks will be executed in current thread
+        self.__device   = Device (astarte_config['device_id'], astarte_config['realm'], astarte_config['credentials_secret'],
+                                    astarte_config['pairing_base_url'], persistency_path, None, astarte_config['ignore_ssl_errors'])
+        self.__device.on_connected                  = self.__astarte_connection_cb
+        self.__device.on_disconnected               = self.__astarte_disconnecton_cb
+        self.__device.on_data_received              = self.__astarte_data_cb
+        self.__device.on_aggregate_data_received    = self.__astarte_aggregated_data_cb
+        
+
         # TODO Registering interfaces
-        # TODO Connecting Astarte client
-        pass
+
+        # Connecting Astarte client
+        self.__device.connect()
+
+
+    def __astarte_connection_cb (self, dvc) :
+        print ('Device connected')
+        self.NewConnectionStatus.emit(True)
+
+
+    def __astarte_disconnecton_cb (self, dvc, boh) :
+        print ("Device disconnected")
+        self.NewConnectionStatus.emit(False)
+
+    
+    def __astarte_data_cb (self, data) :
+        print ("Received server data")
+        # TODO Emite signal
+
+    
+    def __astarte_aggregated_data_cb (self, data) :
+        print ("Received aggregated server data")
+        # TODO Emite signal
+
+
+
+    def is_connected (self) -> bool :
+        return self.__device.is_connected()
