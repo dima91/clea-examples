c331a5022a247f4295e1e232946f2368b8c3ebef
Luca Di Mauro <dima.91@live.it>
2023-02-08 00:21:37 +0100

    [ALL_SVM] Loaded AI models

--- all-seco-smart-vending-machine/edge-app/utils/commons.py
+++ all-seco-smart-vending-machine/edge-app/utils/commons.py
@@ -1,7 +1,8 @@
 
-import os
+import os, cv2
 from enum import Enum
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap, QImage
 
 
 class Status(Enum):
@@ -50,10 +51,18 @@ def create_logger (logging_ns, name):
     return logger
 
 
-
 def get_abs_path (base_folder, file_name) :
     return os.path.abspath(os.path.join(base_folder, file_name))
 
 
 def resize_image (curr_pixmap, target_size) :
     return curr_pixmap.scaled(target_size, Qt.KeepAspectRatio)
+
+
+def cv_img_to_qt_pixmap(cv_img, target_size):
+        """Convert from an opencv image to QPixmap"""
+        h, w, ch        = cv_img.shape
+        bytes_per_line  = ch * w
+        q_img           = QImage(cv_img.data, w, h, bytes_per_line, QImage.Format_RGB888)
+        raw_img         = resize_image(q_img, target_size)
+        return QPixmap.fromImage(raw_img)
