bf88f8689544bd24d79120c67dbb1df01b63c04d
Luca Di Mauro <dima.91@live.it>
2023-03-03 15:48:16 +0100

    [ALL_SVM] Updated suggestions strategies

--- all-seco-smart-vending-machine/edge-app/utils/suggestionsStrategies.py
+++ all-seco-smart-vending-machine/edge-app/utils/suggestionsStrategies.py
@@ -30,26 +30,23 @@ class SuggestionsStrategies(QObject) :
         start_datetime  = now.replace(hour=int(start_schedule_time[:idx]), minute=int(start_schedule_time[idx+1:]), microsecond=0)
         idx             = end_schedule_time.index(":")
         end_datetime    = now.replace(hour=int(end_schedule_time[:idx]), minute=int(end_schedule_time[idx+1:]), microsecond=0)
-        print (f"start:{start_datetime.time()}\tnow:{now.time()}\tend:{end_datetime.time()}")
         return now.time()>start_datetime.time() and now.time()<end_datetime.time()
     
     def __check_date_range(self, start_date, end_date) -> bool:
         now             = datetime.now()
         start_datetime  = parser.parse(start_date)
         end_datetime    = parser.parse(end_date)
-        print (f"start:{start_datetime}\tnow:{now}\tend:{end_datetime}")
         return now.timestamp()>start_datetime.timestamp() and now.timestamp()<end_datetime.timestamp()
     
     def __check_inference_results(self, session, min_age, max_age, target_emotions) -> bool:
         curr_age        = session.inference_results['age']
         curr_emotion    = session.inference_results['emotion']
-        return curr_age>=min_age and curr_age<=max_age and curr_emotion in target_emotions
+        return curr_age>=min_age and curr_age<=max_age and (curr_emotion in target_emotions)
     
 
     def suggest_products(self, session, available_products):
         result      = None
         i_results   = session.inference_results
-        print (f"Retrieving strategies for {session.inference_results}")
         ranges      = self.__startegies[i_results["emotion"]][i_results["gender"]]
         curr_age    = i_results["age"]
         idx         = 0
@@ -76,7 +73,8 @@ class SuggestionsStrategies(QObject) :
                 # Checking age and emotion
                 if not self.__check_inference_results(session, p["minAudienceAge"], p["maxAudienceAge"], p["targetEmotions"]):
                     return False
-            except:
+            except Exception as e:
+                self.__logger.error(f"Cannot filter promo: {e}")
                 return False
             
             return True
@@ -86,7 +84,6 @@ class SuggestionsStrategies(QObject) :
         f_promos    = list(filter(lambda item : promo_filter(available_promos[item], session), available_promos))
         # Randomly choosing an item
         if len(f_promos)!=0:
-            self.__logger.debug(f"Possible promos: {f_promos}")
             random.shuffle(f_promos)
             result  = f_promos[0]
         else:
@@ -104,18 +101,21 @@ class SuggestionsStrategies(QObject) :
                     (not self.__check_date_range(a["startDate"], a["endDate"])):
                     return False
                 # Checking age and emotion
-                if a["targetedAdv"]==True and (not self.__check_inference_results(session, a["minAudienceAge"], a["maxAudienceAge"], a["emotions"])):
-                    return False
-            except:
+                if a["targetedAdv"]==True:
+                    if (not self.__check_inference_results(session, a["minAge"], a["maxAge"], a["emotions"])):
+                        self.__logger.debug("Requirements not satisfied!")
+                        return False
+            except Exception as e:
+                self.__logger.error(f"Cannot filter advertisement: {e}")
                 return False
             return True
-        
+
+
         result  = None
         # Filtering those that can be used
         f_advs  = list(filter(lambda item : adv_filter(available_advertisements[item], session), available_advertisements))
         # Randomly choosing an item
         if len(f_advs)!=0:
-            self.__logger.debug(f"Possible advs: {f_advs}")
             random.shuffle(f_advs)
             result  = f_advs[0]
         else:
