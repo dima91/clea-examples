5998b34131b6e34c0af5b26e2c2d7a405368640d
Luca Di Mauro <dima.91@live.it>
2023-04-07 15:05:55 +0200

    [VPM] Finshd stub implementation with random sensors values

--- ventilation-predictive-maintenance/linux_stub/main.py
+++ ventilation-predictive-maintenance/linux_stub/main.py
@@ -1,22 +1,101 @@
 
-import os, asyncio, json, datetime, time
+import asyncio, time, random
+from datetime import datetime, timedelta
 from argparse import ArgumentParser
 from src.astarteClient import AstarteClient
 
 
-async def coroutine():
-    time.sleep(5.300)
-    print(__name__)
+def generate_random_value(curr, min, max, delta):
+    next_v  = random.uniform(curr-delta, curr+delta)
+    if next_v<min:
+        next_v  = next_v+delta
+    elif next_v>max:
+        next_v  = next_v-delta
+
+    print (f'Next value is {next_v}')
+    return next_v
 
 
-async def main(astarte_client):
+async def simulator(astarte_client):
 
-    astarte_client.connect()
+    LOOP_DELAY_S            = 1
+    EVENTS_BASE_DELAY_S     = 20            # FIXME restore me: 60*8      # 8 minutes
+    EVENTS_DELTA_S          = 5             # FIXME restore me: 60*2      # 2 minutes
+    BASE_EVENT_DURATION_S   = 10.0
+    EVENT_DURATION_DELTA_S  = 5.0
+
+    DELTA_FLOW      = 0.1
+    DELTA_POLLUTION = 5.0
+
+    last_event_timestamp    = datetime.now()
+    event_type              = None
+    event_duration          = 0
+    target_event_value      = 0
+
+    curr_flow       = astarte_client.MAX_FLOW-DELTA_FLOW
+    curr_pollution  = astarte_client.MIN_POLLUTION+DELTA_POLLUTION
     
     while True:
-        time.sleep(2.5)
-        print (f"Connected? {client.is_connected()}")
-        client.send_air_data(1.0, 25.0, 0.0)
+        time.sleep(LOOP_DELAY_S)
+
+        # Checking if events should be created
+        curr_timestamp      = datetime.now()
+        delta_timestamps    = curr_timestamp-last_event_timestamp
+        threshold_delta     = timedelta(seconds=EVENTS_BASE_DELAY_S+random.uniform(-EVENTS_DELTA_S, EVENTS_DELTA_S))
+
+        if delta_timestamps>threshold_delta:
+            print (f"New event: {delta_timestamps} > {threshold_delta}")
+            # Creating the event
+            last_event_timestamp    = curr_timestamp
+            event_type              = random.randint(0,1)   # 0:flow, 1:pollution
+            event_duration          = BASE_EVENT_DURATION_S + random.uniform(-EVENT_DURATION_DELTA_S,\
+                                                                             EVENT_DURATION_DELTA_S)
+            if event_type==0:
+                # Flow event
+                target_event_value  = random.uniform(astarte_client.MIN_FLOW+DELTA_FLOW,\
+                                                     astarte_client.WARNING_FLOW-DELTA_FLOW)
+                curr_flow           = target_event_value
+            else:
+                # Pollution event
+                target_event_value  = random.uniform(astarte_client.WARNING_POLLUTION+DELTA_POLLUTION,\
+                                                     astarte_client.MAX_POLLUTION-DELTA_POLLUTION)
+                curr_pollution      = target_event_value
+                
+            print(f"event_type:{event_type}\ntarget_event_value:{target_event_value}\nevent_duration:{event_duration}")
+        
+        # Generating values for (eventually) existing event
+        if event_type!=None:
+            if event_type==0:
+                print(f"New data for flow event, {delta_timestamps}")
+                curr_flow       = generate_random_value(curr_flow, target_event_value-DELTA_FLOW,\
+                                                target_event_value+DELTA_FLOW, DELTA_FLOW)
+                curr_pollution  = generate_random_value(curr_pollution, astarte_client.MIN_POLLUTION,\
+                                                    astarte_client.MIN_POLLUTION+DELTA_POLLUTION, DELTA_POLLUTION)
+            else:
+                print(f"New data for pollution event, {delta_timestamps}")
+                curr_flow       = generate_random_value(curr_flow, astarte_client.MAX_FLOW-DELTA_FLOW,\
+                                                            astarte_client.MAX_FLOW, DELTA_FLOW)
+                curr_pollution  = generate_random_value(curr_pollution, target_event_value-DELTA_POLLUTION,\
+                                                        target_event_value+DELTA_POLLUTION, DELTA_POLLUTION)
+
+            # Recomputing delta_timestamps value
+            delta_timestamps    = curr_timestamp-last_event_timestamp
+            if delta_timestamps>timedelta(seconds=event_duration):
+                # Clearing event
+                print(f"Clearing event: d_ts{delta_timestamps}, {timedelta(seconds=event_duration)}, {event_duration}")
+                event_type              = None
+                event_duration          = 0
+                target_event_value      = 0
+                last_event_timestamp    = curr_timestamp
+
+        else:
+            curr_flow       = generate_random_value(curr_flow, astarte_client.MAX_FLOW-DELTA_FLOW,\
+                                                astarte_client.MAX_FLOW, DELTA_FLOW)
+            curr_pollution  = generate_random_value(curr_pollution, astarte_client.MIN_POLLUTION,\
+                                                    astarte_client.MIN_POLLUTION+DELTA_POLLUTION, DELTA_POLLUTION)
+
+
+        client.send_air_data(curr_flow, curr_pollution)
 
 
 if __name__== "__main__" :
@@ -31,10 +110,12 @@ if __name__== "__main__" :
 
     loop    = asyncio.get_event_loop()
 
+    # Building AstarteClient object with command line arguments
     client  = AstarteClient(args.device_id, args.realm_name, args.device_secret, args.api_base_url,
                             args.persistency_path, args.interfaces_folder, loop)
+    client.connect()
 
-    loop.create_task (main(client))
+    # Creating simulator task
+    loop.create_task (simulator(client))
 
-    print ("Running loop..")
     loop.run_forever()
--- ventilation-predictive-maintenance/linux_stub/src/astarteClient.py
+++ ventilation-predictive-maintenance/linux_stub/src/astarteClient.py
@@ -7,7 +7,27 @@ from astarte.device import Device
 class AstarteClient :
 
     __AIR_DATA_INTERFACE        = "ai.clea.examples.AirData"
-    __EVENTS_HISTORY_INTERFACE  = "ai.clea.examples.EventsHistory" 
+    __EVENTS_HISTORY_INTERFACE  = "ai.clea.examples.EventsHistory"
+
+    MAX_FLOW        = 1.0
+    MIN_FLOW        = 0.0
+    MAX_SPEED       = 25.0
+    MAX_POLLUTION   = 50.0
+    MIN_POLLUTION   = 0.0
+    
+    WARNING_FLOW        = 0.7
+    __DANGER_FLOW       = 0.5
+    WARNING_POLLUTION   = 20.0
+    __DANGER_POLLUTION  = 35.0
+    __WARNING_NOTE      = 1
+    __DANGER_NOTE       = 2
+    __FLOW_EVENT        = 111
+    __POLLUTION_EVENT   = 222
+
+    __flow_warning_detected         = False
+    __flow_danger_detected          = False
+    __pollution_warning_detected    = False
+    __pollution_danger_detected     = False
 
     __device        = None
     __device_id     = None
@@ -63,6 +83,10 @@ class AstarteClient :
         return f"{self.__api_base_url}/appengine/v1/{self.__realm}/devices/{self.__device_id}"
     
 
+    def __flow_2_speed(self, flow):
+        return self.MAX_SPEED*flow
+    
+
     ##### ================================ #####
 
 
@@ -74,12 +98,60 @@ class AstarteClient :
         return self.__device.is_connected()
     
 
-    def send_air_data(self, flow, velocity, pollution):
-        now = datetime.now()
+    def send_air_data(self, flow, pollution):
+        now     = datetime.now()
+        speed   = self.__flow_2_speed(flow)
         self.__device.send (self.__AIR_DATA_INTERFACE, "/flow", flow, datetime.timestamp(now))
-        self.__device.send (self.__AIR_DATA_INTERFACE, "/velocity", velocity, datetime.timestamp(now))
+        self.__device.send (self.__AIR_DATA_INTERFACE, "/velocity", speed, datetime.timestamp(now))
         self.__device.send (self.__AIR_DATA_INTERFACE, "/pollution", pollution, datetime.timestamp(now))
+
+        # Checking for flow alerts
+        if flow<self.__DANGER_FLOW:
+            if not self.__flow_danger_detected:
+                self.__flow_danger_detected = True
+                self.publish_event(self.__FLOW_EVENT, flow, self.__DANGER_NOTE)
+        else:
+            if flow<self.WARNING_FLOW:
+                if not self.__flow_danger_detected and not self.__flow_warning_detected:
+                    self.__flow_warning_detected    = True
+                    self.publish_event(self.__FLOW_EVENT, flow, self.__WARNING_NOTE)
+            else:
+                self.__flow_danger_detected     = False
+                self.__flow_warning_detected    = False
+
+
+        # Checking for pollution alerts
+        if pollution>self.__DANGER_POLLUTION:
+            if not self.__pollution_danger_detected:
+                self.__pollution_danger_detected    = True
+                self.publish_event(self.__POLLUTION_EVENT, pollution, self.__DANGER_NOTE)
+        else:
+            if pollution>self.WARNING_POLLUTION:
+                if not self.__pollution_danger_detected and not self.__pollution_warning_detected:
+                    self.__pollution_warning_detected    = True
+                    self.publish_event(self.__POLLUTION_EVENT, pollution, self.__WARNING_NOTE)
+            else:
+                self.__pollution_danger_detected    = False
+                self.__pollution_warning_detected   = False
+        
 
 
     def publish_event(self, type, measure, note_code):
-        pass
+        detection   = None
+
+        if type==self.__FLOW_EVENT:
+            detection   = "Flow"
+        elif type==self.__POLLUTION_EVENT:
+            detection   = "Pollution"
+        else:
+            raise Exception(f"Wrong event type: {type}")
+        
+        a_data  = {
+            "detection" : detection,
+            "measure"   : measure,
+            "noteCode"  : note_code
+        }
+
+        print (f"Sending {a_data}")
+        
+        self.__device.send_aggregate(self.__EVENTS_HISTORY_INTERFACE, "/event", a_data)
