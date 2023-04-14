cef94597d3aac6d7c94c34673ea156c16b00e5e5
Luca Di Mauro <dima.91@live.it>
2023-04-14 17:03:23 +0200

    [CMR] Updated docker_builder.sh

--- coffee-machine-retrofitting-example/python-device-stub/docker_builder.sh
+++ coffee-machine-retrofitting-example/python-device-stub/docker_builder.sh
@@ -1,10 +1,5 @@
 
 #|/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Illegal number of parameters: missing image name"
-    exit
-fi
-
-DOCKER_IMAGE_NAME=$1
-docker build -t $DOCKER_IMAGE_NAME -f Dockerfile ..
+cd $(dirname $0)
+docker build -t coffee-machine-retrofitting-stub -f Dockerfile ..
