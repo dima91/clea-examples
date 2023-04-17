809b2b6825628b930d16936ecaf9d7c4d61a56be
Luca Di Mauro <dima.91@live.it>
2023-04-17 10:27:16 +0200

    [VPM] Reoved useless script

--- ventilation-predictive-maintenance/linux_stub/runner.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-
-cd $(dirname $0)
-source venv/bin/activate
-python3 src/main.py -i DQPcm2mQSGaGSjpkoYob7A -s jAhuEsRojW3LUppdB4YEnDOTu6dlLLw6Vrh3uHqF9Oc= \
-                -u https://api.demo.clea.cloud -n showcase -p persistency.d -f ../astarte-interfaces
