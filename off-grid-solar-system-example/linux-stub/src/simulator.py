37c2cf93749c595b5607e633305666bd8ae7c458
Luca Di Mauro <dima.91@live.it>
2023-04-21 16:35:55 +0200

    [OSS] Integrated open-meteo service and handled solar panel, battery and load behaviour

--- off-grid-solar-system-example/linux-stub/src/simulator.py
+++ off-grid-solar-system-example/linux-stub/src/simulator.py
@@ -1,5 +1,6 @@
 
 import time, asyncio
+from typing import Tuple
 from datetime import datetime, timedelta
 
 import utils
@@ -36,33 +37,54 @@ class Simulator:
 
     # TODO Compute the total required power considering board and external appliances
     def __total_load(self) -> float:
-        total_load  = self.__config["BOARD"]["required_power"]
+        total_load  = self.__config["BOARD"]["required_voltage"]*self.__config["BOARD"]["required_current"]
 
         return total_load
     
     # TODO Return total current stats considering board and external appliances
     def __get_load_stats(self):
-        voltage = 0
-        current = 0
+        voltage = self.__config["BOARD"]["required_voltage"]
+        current = self.__config["BOARD"]["required_current"]
 
         return voltage, current
     
 
-    def __update_battery_charge(self, update_value:float) -> None:
-        # TODO Updating battery charge basing on the update_value (in watts)
-        pass
+    # Updates battery charge basing on load requirements and power provided by panel
+    #   Returns current battery stats
+    def __update_battery_charge(self, load_requirements:float, panel_power:float) -> Tuple[float, float, float]:
+        # Updating battery charge basing on the update_value (in watts)
+        battery_config      = self.__config["BATTERY"]
+        remaining_charge    = battery_config["max_charge_watts"]*battery_config["curr_charge_percentage"]
+        remaining_charge += load_requirements
+        remaining_charge += panel_power
 
-    def __get_battery_stats(Self):
-        return 0,0
+        if remaining_charge<=0:
+            return 0,0,0
+
+        # Updating current percentage
+        battery_config["curr_charge_percentage"]    = remaining_charge/battery_config["max_charge_watts"]
+
+        # Computing charge stats
+        charge_delta    = battery_config["max_charge_watts"]-remaining_charge
+        delta_factor    = battery_config["charge_percentage_factor"]*charge_delta
+
+        print(f'remaining_charge:{remaining_charge}, charge_delta:{charge_delta}, delta_factor:{delta_factor}')
+
+        voltage = battery_config["max_voltage"]-delta_factor
+        current = battery_config["max_current"]*(voltage/battery_config["max_voltage"])
+
+        return remaining_charge,voltage,current
 
 
     async def run(self) -> None:
         try :
 
             ext_sensors_publish_delay   = timedelta(seconds=float(self.__config["external_sensors_publish_delay_s"]))
+            stats_publish_delay         = timedelta(seconds=float(self.__config["stats_publish_delay_s"]))
 
             now                             = datetime.now()
             last_ext_sensors_publish_time   = now
+            last_stats_publish_time         = now
 
             while True:
                 await asyncio.sleep(5)
@@ -70,37 +92,48 @@ class Simulator:
 
                 # Publishing external sensors values
                 if (now-last_ext_sensors_publish_time)>ext_sensors_publish_delay:
+                    print('Publishing external sensors values..')
                     last_ext_sensors_publish_time   = now
                     self.__client.publish_day_period(self.__weather_collector.current_day_period())
                     self.__client.publish_temperature(self.__weather_collector.current_temperature())
                     self.__client.publish_wind_speed(self.__weather_collector.current_wind_speed())
-                    self.__client.publish_reference_current(self.__weather_collector.reference_solar_power(self.__solar_panel["max_power"])) # FIXME Pass a different max power
+                    self.__client.publish_reference_current(round(self.__weather_collector.solar_power_to_watts(self.__config["max_reference_solar_power"]),2))
 
                 # Computing generated power by solar panel
-                actual_panel_power      = self.__weather_collector.reference_solar_power(self.__solar_panel["max_power"])
+                actual_panel_power      = self.__weather_collector.solar_power_to_watts(self.__solar_panel["max_power"])
                 remaining_panel_power   = actual_panel_power
                 total_load              = self.__total_load()
-                if actual_panel_power>=total_load:
-                    # TODO Providing power supply from solar panel
+                if remaining_panel_power>=total_load:
+                    # Providing power supply from solar panel
+                    print("Power supply from PANEL")
                     remaining_panel_power -= total_load
+                    total_load  = 0
                 else :
-                    # TODO Providing power supply from battery charge -> decreasing battery charge!
-                    self.__update_battery_charge(-total_load)
+                    # Providing power supply from battery charge
+                    print("Power supply from BATTERY")
+                    pass
 
-                # TODO Remaining power supply goes to battery
-                print (f"{remaining_panel_power}W goes to battery")
-                self.__update_battery_charge(actual_panel_power)
+                # Updating battery charge
+                remaining_charge,voltage, current   = self.__update_battery_charge(-total_load, remaining_panel_power)
+                print (f"{remaining_panel_power}/{actual_panel_power} W goes to battery")
 
+                if remaining_charge<=0:
+                    # No data will be published!
+                    pass
+                elif (now-last_stats_publish_time)>stats_publish_delay:
+                    print('Publishing stats values..')
+                    last_stats_publish_time = now
 
-                # TODO Publishing panel, battery and load stats
-                voltage,current = self.__get_panel_stats(actual_panel_power)
-                self.__client.publish_panel_stats(voltage, current)
-                
-                voltage,current = self.__get_battery_stats()
-                self.__client.publish_battery_stats(voltage, current)
-                
-                voltage,current = self.__get_load_stats()
-                self.__client.publish_load_stats(voltage, current)
+                    # Publishing battery, battery and load stats
+                    self.__client.publish_battery_stats(voltage, current)
+
+                    voltage,current = self.__get_panel_stats(actual_panel_power)
+                    self.__client.publish_panel_stats(voltage, current)
+                    
+                    voltage,current = self.__get_load_stats()
+                    self.__client.publish_load_stats(voltage, current)
+
+                print("=====\n\n")  #FIXME Remove me!
 
 
         except Exception as e:
--- off-grid-solar-system-example/linux-stub/src/weatherCollector.py
+++ off-grid-solar-system-example/linux-stub/src/weatherCollector.py
@@ -1,5 +1,5 @@
 
-import time, json, asyncio, requests
+import os, json, asyncio, requests, math
 from datetime import datetime
 from typing import Tuple
 
@@ -22,35 +22,37 @@ class WeatherCollector:
 
     
     def __get_sunrise_sunset_times(self) -> Tuple[datetime, datetime]:
-        time_format = "%I:%M %p"
-        return datetime.strptime(self.__last_weather_data["weather"][0]["astronomy"][0]["sunrise"], time_format),\
-                datetime.strptime(self.__last_weather_data["weather"][0]["astronomy"][0]["sunset"], time_format)
+        time_format = "%Y-%m-%dT%H:%M"
+        daily       = self.__last_weather_data["daily"]
+        return datetime.strptime(daily["sunrise"][0], time_format),datetime.strptime(daily["sunset"][0], time_format)
 
     
     def cloud_cover_percentage(self) -> float:
-        cloudcover  = float(self.__last_weather_data["current_condition"][0]["cloudcover"])     # FIXME Is 0 the correct index?
-        return cloudcover/100
+        now = datetime.now()
+        return (float(self.__last_weather_data["hourly"]["cloudcover"][now.hour]))/100
 
 
     def current_temperature(self) -> float:
-        return float(self.__last_weather_data["current_condition"][0]["temp_C"])
+        return float(self.__last_weather_data["current_weather"]["temperature"])
 
     def current_wind_speed(self) -> float:
-        speed   = float(self.__last_weather_data["current_condition"][0]["windspeedKmph"])
-        return round(self.__KMPH_to_MPS(speed), 2)
+        speed   = float(self.__last_weather_data["current_weather"]["windspeed"])
+        return round(speed, 2)
 
     def current_day_period(self) -> DayPeriod:
         # Computing day period basing on current time and sunrise and sunset hours
-        sunrise_t, sunset_t = self.__get_sunrise_sunset_times()
-        now                 = datetime.now()
+        # sunrise_t, sunset_t = self.__get_sunrise_sunset_times()
+        # now                 = datetime.now()
 
-        if sunrise_t.time() <= now.time() <= sunset_t.time():
-            return DayPeriod(1)
-        else:
-            return DayPeriod(0)
+        # if sunrise_t.time() <= now.time() <= sunset_t.time():
+        #     return DayPeriod(1)
+        # else:
+        #     return DayPeriod(0)
+        return DayPeriod(self.__last_weather_data["current_weather"]["is_day"])
 
-    def reference_solar_power(self, max_power) -> float:
+    def solar_power_to_watts(self, max_power) -> float:
         if self.current_day_period() == DayPeriod.NIGHT:
+            print("It's night")
             return 0
         
         # It depends also on day hour respect to sunset and sunrise: sqrt(day_percentage)
@@ -61,32 +63,46 @@ class WeatherCollector:
         sunrise_minutes     = (sunrise_t.hour*60)+sunrise_t.minute
         sunset_minutes      = (sunset_t.hour*60)+sunset_t.minute
         solar_minutes       = sunset_minutes-sunrise_minutes
-        curr_ref_minutes    = (now.hour*60)+now.minute-sunrise_minutes
+        curr_minutes_norm   = (now.hour*60)+now.minute-sunrise_minutes
         day_percentage      = 1
 
-        if curr_ref_minutes<(solar_minutes/2):
-            day_percentage  = curr_ref_minutes/(solar_minutes/2)
+        if curr_minutes_norm<(solar_minutes/2):
+            day_percentage  = curr_minutes_norm/(solar_minutes/2)
         else:
-            day_percentage  = (curr_ref_minutes-(solar_minutes/2))/(solar_minutes/2)
+            day_percentage  = 1-((curr_minutes_norm-(solar_minutes/2))/(solar_minutes/2))
+        day_percentage  = math.sqrt(day_percentage)
+        
+        print(f'sunrise_minutes:{sunrise_minutes}, sunset_minutes:{sunset_minutes}, solar_minutes:{solar_minutes}, curr_minutes_norm:{curr_minutes_norm}, cloud_percentage:{cloud_percentage}, day_percentage:{day_percentage}')
 
-        print(f"curr_ref_minutes:{curr_ref_minutes}")
-        print(f"solar_minutes:{solar_minutes}")
-        print(f"day_percentage:{day_percentage}")
-
-        return max_power*cloud_percentage*day_percentage
+        return (max_power-(max_power*cloud_percentage))*day_percentage
 
 
     async def run(self) -> None:
         try :
-            loop_delay  = float(self.__config["WEATHER_PARAMS"]["loop_delay_s"])
-            city        = self.__config["WEATHER_PARAMS"]["city"]
+            weather_config  = self.__config["WEATHER_PARAMS"]
+            loop_delay      = float(weather_config["loop_delay_s"])
+
 
             while True:
-                now                         = datetime.now()
-                weather_data                = requests.get(f"https://wttr.in/{city}?format=j1")
-                self.__last_weather_data    = weather_data.json()
-                
-                await asyncio.sleep(loop_delay)
+                try :
+                    now                 = datetime.now()
+                    open_meteo_query    = {
+                        "latitude"          : weather_config["latitude"],
+                        "longitude"         : weather_config["longitude"],
+                        "hourly"            : "temperature_2m,cloudcover",
+                        "daily"             : "sunrise,sunset",
+                        "windspeed_unit"    : "ms",
+                        "current_weather"   : True,
+                        "forecast_days"     : 1,
+                        "timezone"          : os.environ["TZ"]
+                    }
+                    weather_data        = requests.get("https://api.open-meteo.com/v1/forecast", timeout=loop_delay/2,
+                                                       params=open_meteo_query)
+                    self.__last_weather_data    = weather_data.json()
+                    await asyncio.sleep(loop_delay)
+
+                except requests.exceptions.RequestException as err:
+                    print(f"Requests error: {err}")
 
         except Exception as e:
             print (f"[WC] Catched this exception: {e}")
