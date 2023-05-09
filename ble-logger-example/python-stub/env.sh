71ec798fe67010ab098aece46a75cb0c4348c509
Luca Di Mauro <dima.91@live.it>
2023-05-09 10:00:37 +0200

    [BLE] Removed credentials information

--- ble-logger-example/python-stub/env.sh
+++ ble-logger-example/python-stub/env.sh
@@ -2,10 +2,10 @@
 PYTHONUNBUFFERED=1
 
 # Astarte details
-DEVICE_ID=G43UvIu2TguNVHZ1I61HYw
-DEVICE_SECRET=DC3Zi42gNp4mwPasfDBU2olk9rlrq5ZbG8f9wpjjMg4=
-API_BASE_URL=https://api.eu1.astarte.cloud
-REALM_NAME=tester
+DEVICE_ID=
+DEVICE_SECRET=
+API_BASE_URL=
+REALM_NAME=
 PERSISTENCY_PATH=persistency.d
 INTERFACES_FOLDER=astarte-interfaces
 
